c379288fcd210761896eab5c242faaa4620b8eb6
Anirudh Bandi <anirudhbandi@Anirudhs-MBP.home>
2018-01-06 13:10:35 -0500

    player model created

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,14 +9,41 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player : Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        nextBtn.isEnabled = false
+        player  = Player()
 
         // Do any additional setup after loading the view.
     }
-
-   
+    
+    @IBAction func onMensTapped(_ sender: BorderButton) {
+        
+        selectLeague(leagueSelected: "mens")
+        
+    }
+    
+    @IBAction func onWomensTapped(_ sender: BorderButton) {
+        selectLeague(leagueSelected: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: BorderButton) {
+        
+        selectLeague(leagueSelected: "coed")
+        
+    }
+    
+    func selectLeague(leagueSelected : String) {
+        player.desiredLeague = leagueSelected
+        nextBtn.isEnabled = true
+    }
     @IBAction func onNextTapped(_ sender: BorderButton) {
         
         performSegue(withIdentifier: "skillVCSegue", sender: self)
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Anirudh Bandi on 1/6/18.
+//  Copyright © 2018 Anirudh Bandi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague : String?
+    var selectedSkillLevel : String?
+}
